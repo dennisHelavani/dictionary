2d7ff8df195bb8538412ae9df5e80e0d18fc7792
Dzianis Helavani <99481760+dennisHelavani@users.noreply.github.com>
2022-05-18 23:52:00 +0300

    Update Synonym.js

--- src/Components/Synonym.js
+++ src/Components/Synonym.js
@@ -1,6 +1,6 @@
 const Synonym = ({ mean }) => {
   return (
-    <div className="columns-2 md:columns-2">
+    <div className="columns-2 md:columns-3">
       {mean.map((val) =>
         val.meanings.map((means) =>
           means.synonyms.map((syn, i) => {
